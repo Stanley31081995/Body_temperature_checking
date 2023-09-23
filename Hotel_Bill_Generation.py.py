f0e598b49723f23d5d66bfaba17458ba4a642c32
Stanley Abraham paul <122020499+Stanley31081995@users.noreply.github.com>
2023-09-23 21:08:54 +0530

    Add files via upload

---
+++ Hotel_Bill_Generation.py.py
@@ -0,0 +1,17 @@
+#Temperature checking
+#__________________
+
+temp=float(input('Enter body temperature: ')) #user input
+
+if temp>98.4:
+    print('High Fever')
+
+elif temp<98.4:
+    print('Cold Fever') 
+
+else:
+    print('Normal')
+
+
+
+
